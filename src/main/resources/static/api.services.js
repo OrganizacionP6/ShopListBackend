72ac6d7206cbfc26580b4ce7121835fd7aebe409
FranGarciaga <frangg_10@hotmail.com>
2024-11-22 11:56:51 +0100

    feat: connect JS

--- src/main/resources/static/api.services.js
+++ src/main/resources/static/api.services.js
@@ -1,24 +1,24 @@
 const URL_BASE = "http://localhost:8080/api/products";
 
 export async function getAllItemsFromAPI() {
-  const response = await fetch(URL_BASE + "/ingredients");
+  const response = await fetch(URL_BASE);
   const data = await response.json();
   return data;
 }
 export async function postItemToAPI(item) {
-  const response = await fetch(URL_BASE + "/ingredients", {
+  const response = await fetch(URL_BASE, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(item),
   });
 }
 export async function deleteItemFromAPI(id) {
-  const response = await fetch(URL_BASE + "/ingredients/" + id, {
+  const response = await fetch(`${URL_BASE}/${id}`, {
     method: "DELETE",
   });
 }
 export async function putItemToAPI(id, item) {
-  const response = await fetch(URL_BASE + "/ingredients/" + id, {
+  const response = await fetch(`${URL_BASE}/${id}`, {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(item),
